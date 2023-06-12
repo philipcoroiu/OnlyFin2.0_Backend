2c477e39630a07602c3ad1189bd1d68591b62a75
Johannes <98045373+Luxcorel@users.noreply.github.com>
2023-06-12 19:21:50 +0200

    Setup project using Spring Initializr

---
+++ onlyfin2-backend/src/test/java/se/onlyfin/onlyfin2backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.onlyfin.onlyfin2backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
