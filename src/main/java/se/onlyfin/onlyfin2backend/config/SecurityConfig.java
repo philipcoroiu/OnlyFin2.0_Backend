de58a0cb3486d07cab24ea1a10adf37d87dacb2f
Johannes <98045373+Luxcorel@users.noreply.github.com>
2023-06-28 23:22:51 +0200

    Removed H2 leftovers

--- src/main/java/se/onlyfin/onlyfin2backend/config/SecurityConfig.java
+++ src/main/java/se/onlyfin/onlyfin2backend/config/SecurityConfig.java
@@ -10,16 +10,12 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 /**
  * This class is used to configure the security settings in Spring Security.
@@ -40,14 +36,7 @@ public class SecurityConfig {
 
         http.csrf(AbstractHttpConfigurer::disable);
 
-        //WARNING: SHOULD BE DISABLED IN PRODUCTION!
-        //Required to enable H2 development console
-        http.headers(headersConfigurer -> headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
-
         http.authorizeHttpRequests(auth -> auth
-                //WARNING: DISABLE IN PRODUCTION - allow H2 development console:
-                .requestMatchers(toH2Console()).permitAll()
-
                 //allow OPTION requests
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 
@@ -85,8 +74,7 @@ public class SecurityConfig {
      */
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-        //TODO: Use this later: return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 
     /**
